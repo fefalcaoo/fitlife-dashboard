5e6b36f76cea77efc0f8f386f2dda28c1fed8f45
fefalcaoo <fefalcaooferreira@gmail.com>
2026-05-18 21:10:00 -0300

    Projeto FitLife Finalizado

--- js/tmb.js
+++ js/tmb.js
@@ -1,29 +1,30 @@
-function calculeteTMB(event) {
-
+function calculateTMB(event) {
     event.preventDefault()
 
-    const gender = document.getElementById('gender').value
-    const weight = parceFloat(document.getElementById('weight').value)
-    const height = parceInt(document.getElementById('height').value)
-    const age = parceInt(document.getElementById('age').value)
+    const gender = document.getElementById("gender").value
+
+    const age = parseInt(document.getElementById('age').value)
+    const height = parseInt(document.getElementById('height').value)
+
+    const weight = parseFloat(document.getElementById('weight').value)
 
     const resultBox = document.getElementById('result')
     const valueText = document.getElementById('tmb-value')
 
-if (!weight || !height || !age) {
-    alert("Por favor, preencha todos os campos com valores válidos")
-    return
+    if(!weight || !age || !height) {
+        alert("Preencha todos os campos corretamente!")
+        return
     }
 
     let tmb = 0
 
     if (gender === 'male') {
-        tmb = 88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)
-    } else {
-        tmb = 447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)
+        tmb = 88.36 + 13.4 * weight + 4.8 * height -5.7 * age
+    } else { 
+        tmb = 447.6 + 9.2 * weight + 3.1 * height -4.3 * age
     }
 
     valueText.innerText = Math.round(tmb)
 
-    resultBox.classList.add('show')
+    resultBox.classList.add("show")
 }
